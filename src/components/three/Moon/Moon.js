00fd4a48fd0fc1712cfef5c5e137f0d6d59d4969
GouWantong <gouwantong@live.com>
2017-09-01 14:51:32 +0800

    add react motion

--- src/components/three/Moon/Moon.js
+++ src/components/three/Moon/Moon.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Motion, spring } from 'react-motion';
 import injectSheet from 'react-jss';
 import classnames from 'classnames';
 import createMoon from './MoonThree';
@@ -31,6 +32,7 @@ class Moon extends Component {
     this.moon = createMoon(this.element);
     this.moon.init();
     this.moon.animate();
+    this.handleLightAnimate();
   }
 
   componentWillUnmount() {
@@ -40,24 +42,53 @@ class Moon extends Component {
   handlePrimaryLightPosition = (x, y, z) =>
     this.setState({ primaryLightX: x, primaryLightY: y, primaryLightZ: z });
 
+  handleLightAnimate = () => {
+    const time = Date.now() / 500;
+    const x = Math.sin(time);
+    const z = Math.cos(time);
+    this.setState({
+      primaryLightX: x,
+      primaryLightZ: z
+    });
+    requestAnimationFrame(this.handleLightAnimate);
+  };
+
   render() {
     const { classes } = this.props;
     const { primaryLightX, primaryLightY, primaryLightZ } = this.state;
     const { cameraX, cameraY, cameraZ } = this.state;
+    const moon = this.moon && this.moon;
     const classNames = classnames({
       [classes.root]: true,
       [classes.done]: this.state.done
     });
 
-    this.moon &&
-      this.moon.setPrimaryLightPosition(
-        primaryLightX,
-        primaryLightY,
-        primaryLightZ
-      );
-    this.moon && this.moon.setCameraPosition(cameraX, cameraY, cameraZ);
+    moon &&
+      moon.setPrimaryLightPosition(primaryLightX, primaryLightY, primaryLightZ);
 
-    return <div className={classNames} ref={e => (this.element = e)} />;
+    moon && moon.setCameraPosition(cameraX, cameraY, cameraZ);
+
+    return (
+      <Motion
+        style={{
+          cameraX: spring(this.state.cameraX),
+          cameraY: spring(this.state.cameraY),
+          cameraZ: spring(this.state.cameraZ)
+        }}
+      >
+        {({
+          cameraX,
+          cameraY,
+          cameraZ,
+          primaryLightX,
+          primaryLightY,
+          primaryLightZ
+        }) => {
+          moon && moon.setCameraPosition(cameraX, cameraY, cameraZ);
+          return <div className={classNames} ref={e => (this.element = e)} />;
+        }}
+      </Motion>
+    );
   }
 }
 
