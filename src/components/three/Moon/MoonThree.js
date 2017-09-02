931b195aabc15e0b352d6b1dda410a003a5fa0b7
Wantong Go <gouwantong@live.com>
2017-09-02 21:07:49 +0800

    调整配置

--- src/components/three/Moon/MoonThree.js
+++ src/components/three/Moon/MoonThree.js
@@ -6,10 +6,13 @@ export default function createModel(element: HTMLElement) {
 
   let primaryLight, secondaryLight;
   let controls;
+  let clock;
 
   function init() {
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0x242424);
+
+    clock = new THREE.Clock();
 
     camera = new THREE.PerspectiveCamera(
       15,
@@ -38,9 +41,9 @@ export default function createModel(element: HTMLElement) {
     scene.add(primaryLight);
 
     // 辅助光
-    const secondaryLight = new THREE.DirectionalLight(0xcccccc);
-    secondaryLight.position.set(0, 50, -100);
-    scene.add(secondaryLight);
+    // const secondaryLight = new THREE.DirectionalLight(0xcccccc);
+    // secondaryLight.position.set(0, 50, -100);
+    // scene.add(secondaryLight);
 
     // 导入 json 格式模型
     const loader = new THREE.JSONLoader();
@@ -50,7 +53,7 @@ export default function createModel(element: HTMLElement) {
       // load 完成后的回调
       (geometry, materials) => {
         const material = materials[0];
-        material.emissive.setHex(0x202015);
+        material.emissive.setHex(0x242424);
         const object = new THREE.Mesh(geometry, material);
         scene.add(object);
       }
@@ -84,6 +87,7 @@ export default function createModel(element: HTMLElement) {
 
   return {
     init,
+    clock,
     render,
     animate,
     setPrimaryLightPosition,
