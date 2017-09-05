42acf37be0a1ca387124a1f26ae1341df3ea1d1a
GouWantong <gouwantong@live.com>
2017-09-05 18:37:00 +0800

    add transition hoc

--- src/components/HOC/fadeTransition.js
+++ src/components/HOC/fadeTransition.js
@@ -1,2 +1,38 @@
+// @flow
+
 import React, { Component } from 'react';
-import { TransitionMotion, spring } from 'react-motion';
+import type { ComponentType } from 'react';
+import { Motion, spring } from 'react-motion';
+
+type Options = {
+  stiffness?: number,
+  damping?: number,
+  precision?: number
+};
+
+type Props = {};
+type State = {
+  open: boolean
+};
+
+export default function fadeTransition(options?: Options = {}) {
+  const { stiffness, damping, precision } = options;
+
+  function enhance(BaseComponent: ComponentType<any>) {
+    class Transition extends Component<Props, State> {
+      state = { open: false };
+      render() {
+        return (
+          <Motion style={{ x: spring(this.state.open ? 400 : 0) }}>
+            {({ x }) =>
+              <BaseComponent
+                style={{ transform: `translate3d(${x}px, 0, 0)` }}
+              />}
+          </Motion>
+        );
+      }
+    }
+    return Transition;
+  }
+  return enhance;
+}
--- src/components/Home/MainHeader.js
+++ src/components/Home/MainHeader.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import injectSheet from 'react-jss';
 import Beer from 'react-icons/lib/fa/beer';
+import fadeTransition from '../HOC/fadeTransition';
 
 const styles = theme => ({
   root: {
@@ -61,4 +62,7 @@ class MainHeader extends Component {
   }
 }
 
-export default injectSheet(styles)(MainHeader);
+const styledMainHeader = injectSheet(styles)(MainHeader);
+
+export default fadeTransition()(styledMainHeader);
+// export default styledMainHeader;
